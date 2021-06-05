6ccd25e344b128787192c7e58e6bcf10f36526b4
Duy Vo <73686926+duyvo209@users.noreply.github.com>
2021-06-05 18:44:56 +0700

    Delete configs.dart

--- lib/configs/configs.dart
+++
@@ -1,8 +0,0 @@
-// ignore: non_constant_identifier_names
-// final String GOOGLE_API_KEY = 'AIzaSyBLnQ6KLSCfkgMFDgbw1_jMzlo4fhXILss';
-
-// ignore: non_constant_identifier_names
-final String GOOGLE_API_KEY = 'AIzaSyBHRMxpBKc25CMHY51h1jrnCCm6PjNs62s';
-
-// ignore: non_constant_identifier_names
-// final String GOOGLE_API_KEY = 'AIzaSyAuFYSUAOPOxPyHolLKH8yNENjBipVAKCM';
